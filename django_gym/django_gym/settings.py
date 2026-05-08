9bc4792b5316cb3585fc7d28379159a1fc09070b
DVPONTE <DPONTE@ANGKORPERU.COM>
2026-05-08 05:10:25 -0500

    fix: debug DATABASE_URL check v2

--- django_gym/django_gym/settings.py
+++ django_gym/django_gym/settings.py
@@ -3,6 +3,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+DATABASE_URL_CHECK = os.getenv('DATABASE_URL')
+print(f"=== DATABASE_URL existe: {bool(DATABASE_URL_CHECK)} ===")
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +129,26 @@ SPECTACULAR_SETTINGS = {
 }
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+import dj_database_url
+
+DATABASE_URL = os.getenv('DATABASE_URL')
+
+if DATABASE_URL:
+    DATABASES = {
+        'default': dj_database_url.config(
+            default=DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': os.getenv('DB_NAME'),
+            'USER': os.getenv('DB_USER'),
+            'PASSWORD': os.getenv('DB_PASSWORD'),
+            'HOST': os.getenv('DB_HOST'),
+            'PORT': os.getenv('DB_PORT'),
+        }
+    }
